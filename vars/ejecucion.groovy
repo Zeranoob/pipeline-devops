4f622cfc4126681e61e1bd338d35064c9f7d91a4
Zeranoob <diperezvizcarra@gmail.com>
2020-12-19 11:29:12 -0300

    migracion de archivos desde feature-library

---
+++ vars/ejecucion.groovy
@@ -0,0 +1,30 @@
+def call(){
+  
+pipeline {
+        agent any
+
+        parameters { choice(name: 'herramienta', choices: ['gradle','maven'], description: '') }
+
+        stages {
+                stage('Pipelines') {
+                        steps {
+                                script {
+
+                                        params.herramienta // -> gradle o maven
+
+                                        if(params.herramienta == 'gradle'){
+                        def ejecucion = load 'gradle.groovy'
+                        ejecucion.call()
+                    }else{
+                        def ejecucion = load 'maven.groovy'
+                        ejecucion.call()
+                    }
+                                }
+                        }
+                }
+        }
+  }
+
+}
+
+return this;
