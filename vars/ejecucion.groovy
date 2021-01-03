965ddcb00587abc74af07c0509e5b408d9b46f23
Zeranoob <diperezvizcarra@gmail.com>
2021-01-03 16:24:18 -0300

    pipeline devops

--- vars/ejecucion.groovy
+++ vars/ejecucion.groovy
@@ -2,16 +2,12 @@ def call(){
   
 pipeline {
         agent any
-
-        parameters { choice(name: 'herramienta', choices: ['gradle','maven'], description: '') }
-
+        parameters { choice(name: 'herramienta', choices: ['gradle','maven'], description: 'Elección de herramienta de construcción para aplicación covid') }
         stages {
                 stage('Pipelines') {
                         steps {
                                 script {
-
                                         params.herramienta // -> gradle o maven
-
                                         if(params.herramienta == 'gradle'){
                         def ejecucion = load 'gradle.groovy'
                         ejecucion.call()
@@ -19,12 +15,18 @@ pipeline {
                         def ejecucion = load 'maven.groovy'
                         ejecucion.call()
                     }
-                                }
+                }
                         }
                 }
         }
-  }
-
-}
+         post {
+                success {
+                        slackSend teamDomain: 'devops-usach-2020', tokenCredentialId: 'token-slack', color: 'good', message: "Diego Perez][Pipeline-maven-gradle][${params.herramienta}] Ejecución exitosa."
+                }
+                failure {
+                        slackSend teamDomain: 'devops-usach-2020', tokenCredentialId: 'token-slack', color: 'danger', message: "[Diego Perez][Pipeline-maven-gradle][${params.herramienta}] Ejecución fallida en stage ${TAREA}."
+                                }
+            }
+        }
 
 return this;
--- vars/maven.groovy
+++ vars/maven.groovy
@@ -1,22 +1,22 @@
 def call()  {
   
     stage('Compile') {
-         
+          env.TAREA = 'Compile'
           sh 'mvn clean compile -e'
           
          }
         stage('Test') {
-         
+         env.TAREA = 'Test'
           sh 'mvn clean test -e'
         
         } 
       stage('Jar') {
-       
+        env.TAREA = 'Jar'  
          sh 'mvn clean package -e'
       }
       
       stage('sonar') {
-        
+        env.TAREA = 'Sonar'
           withSonarQubeEnv(installationName: 'Sonar') {    
            sh 'mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.7.0.1746:sonar' 
         
@@ -24,7 +24,7 @@ def call()  {
       }  
 
       stage('uploadNexus') {
-         
+         env.TAREA = 'uploadNexus'
           nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'build/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
          
        }   
