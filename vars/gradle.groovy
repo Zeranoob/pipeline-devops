af05c1f9463fc22324bce524f92e963032833274
Zeranoob <diperezvizcarra@gmail.com>
2021-01-04 00:54:45 -0300

    add new stage

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -1,5 +1,5 @@
 def call(){
-    def stages = ["build & test", "sonar", "run", "rest", "nexus"] as String[]
+    def stages = ["Build & Test", "Sonar", "Run", "Rest", "Nexus"] as String[]
     def userStages
 
     if(params.stage != ''){
@@ -8,37 +8,37 @@ def call(){
      
     if(userStages.every { stages.contains(it) }){
     for(stg in userStages){
-        if(stg.contains("build & test")){
-            stage('build & test'){ 
-                env.TAREA = env.STAGE_NAME
-                sh "./gradle clean build"
+        if(stg.contains("Build & Test")){
+            stage('Build & Test'){ 
+                env.TAREA = Build & Test
+                sh "gradle clean build"
     }
         }
-        if(stg.contains("sonar")){
-            stage('sonar'){
-               env.TAREA = env.STAGE_NAME
+        if(stg.contains("Sonar")){
+            stage('Sonar'){
+               env.TAREA = Sonar
                def scannerHome = tool 'sonar-scanner';
                withSonarQubeEnv('Sonar'){ 
                    sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build"
         }  
     }
         }
-        if(stg.contains("run")){
-            stage('run'){
-                env.TAREA = env.STAGE_NAME
+        if(stg.contains("Run")){
+            stage('Run'){
+                env.TAREA = Run
                 sh 'nohup bash gradle bootRun &'
-                sleep 15
+                sleep 5
     }
         }
-        if(stg.contains("rest")){
-            stage('rest'){
-                env.TAREA = env.STAGE_NAME
+        if(stg.contains("Rest")){
+            stage('Rest'){
+                env.TAREA = Rest
                 sh 'curl -X GET http://localhost:8081/rest/mscovid/test?msg=testing'
     }
         }
-        if(stg.contains("nexus")){
-            stage('nexus'){
-                env.TAREA = env.STAGE_NAME
+        if(stg.contains("Nexus")){
+            stage('Nexus'){
+                env.TAREA = Nexus
                 nexusPublisher nexusInstanceId: 'Nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'build/libs/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]    
     }
         }
