5ad7da5cc0053ae275801f7658cc279610736b04
Zeranoob <diperezvizcarra@gmail.com>
2021-01-05 18:24:37 -0300

    fix

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -59,7 +59,7 @@ def call()  {
     if(_stage.contains('Nexus')) {
         stage('Nexus') {
             env.LAST_STAGE_NAME = env.STAGE_NAME
-            nexusPublisher nexusInstanceId: 'Nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'build/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
+            nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'build/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
         }
     }
 }
